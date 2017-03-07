a6b53c9b5739fe36d371e6f5ec5125a115770582
Daniel Stahl <danstahl1138@gmail.com>
2017-03-06 19:14:42 -0600

    fixed makefile

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 set mypath=%cd%
-cmd /k "cd \Program Files (x86)\Microsoft Visual Studio 14.0\VC & vcvarsall amd64 & cd /d %mypath% & cl /EHsc  /O2 test.cpp /I ..\FangOost /I ..\rapidjson\include\rapidjson /I ..\FunctionalUtilities & exit 0"
+cmd /k "cd \Program Files (x86)\Microsoft Visual Studio 14.0\VC & vcvarsall amd64 & cd /d %mypath% & cl /EHsc  /O2 main.cpp /I ..\FangOost /I ..\rapidjson\include\rapidjson /I ..\FunctionalUtilities & exit 0"
